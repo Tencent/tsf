c3d53d7ce2d85e45119b007e9cfb359f1762ee70
markyuan@tencent.com <syyuanyizhi@163.com>
2015-07-04 21:54:48 +0800

    Update TestController.php
    添加 udp tcp  http 以及mutical的示例代码

--- examples/src/controller/TestController.php
+++ examples/src/controller/TestController.php
@@ -16,6 +16,61 @@ class TestController extends Controller{
 		SysLog::debug(__METHOD__ ." res  == ".print_r($res, true), __CLASS__);
 		$response ->end(" test response ");
 	}
+	
+	public function actionProtocol(){
+		
+		$tcpReturn=(yield $this->tcpTest());
+  		$udpReturn=(yield $this->udpTest());
+  		$httpReturn=(yield $this->httpTest());
+  		$response ->end(" test response ");
+	}
+
+	public function actionMuticall(){
+  		$res = (yield $this->muticallTest());
+  		$response ->end(" test response ");
+	}
+
+
+	public function tcpTest(){
+	    $ip = '127.0.0.1';
+	    $port = '9905';
+	    $data = 'test';
+	    $timeout = 0.5; //second
+	    yield new Swoole\Client\TCP($ip, $port, $data, $timeout);
+	 }
+
+	  public function udpTest(){
+	    $ip = '127.0.0.1';
+	    $port = '9905';
+	    $data = 'test';
+	    $timeout = 0.5; //second
+	    yield new Swoole\Client\UDP($ip, $port, $data, $timeout);
+	  }
+
+	  public function httpTest(){
+	    $url='http://www.qq.com';
+	    $httpRequest= new Swoole\Client\HTTP($url);
+	    $data='testdata';
+	    $header = array(
+	      'Content-Length' => 12345,
+	    );
+	    yield $httpRequest->get($url); //yield $httpRequest->post($path, $data, $header);
+	  }
+	  
+	  
+	  public function muticallTest(){
+	    $ip = '127.0.0.1';
+	    $port = '9905';
+	    $data = 'test';
+	    $timeout = 0.5; //second
+	    $calls=new Swoole\Client\Multi();
+	    $firstReq=new Swoole\Client\TCP($ip, $port, $data, $timeout);
+	    $secondReq=new Swoole\Client\UDP($ip, $port, $data, $timeout);
+	    $calls ->request($firstReq,'first');             //first request
+	    $calls ->request($secondReq,'second');             //second request
+	    yield $calls;
+	  }
+
 
 
 	private function test(){
