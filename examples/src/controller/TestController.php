af53d42ff0c9b927d5f9539b746bd8bacf4a1726
winterswang <paradox0504@gmail.com>
2015-07-05 10:46:40 +0800

    update coroutine schedule

--- examples/src/controller/TestController.php
+++ examples/src/controller/TestController.php
@@ -3,7 +3,7 @@
  * @Author: winterswang
  * @Date:   2015-07-03 17:49:43
  * @Last Modified by:   winterswang
- * @Last Modified time: 2015-07-03 19:36:08
+ * @Last Modified time: 2015-07-04 23:46:34
  */
 
 class TestController extends Controller{
@@ -15,68 +15,13 @@ class TestController extends Controller{
 		$res =(yield $this ->test());
 		SysLog::debug(__METHOD__ ." res  == ".print_r($res, true), __CLASS__);
 		$response ->end(" test response ");
+		yield Swoole\Coroutine\SysCall::end('test for syscall end');
 	}
 	
-	public function actionProtocol(){
-		
-		$tcpReturn=(yield $this->tcpTest());
-  		$udpReturn=(yield $this->udpTest());
-  		$httpReturn=(yield $this->httpTest());
-  		$response ->end(" test response ");
-	}
-
-	public function actionMuticall(){
-  		$res = (yield $this->muticallTest());
-  		$response ->end(" test response ");
-	}
-
-
-	public function tcpTest(){
-	    $ip = '127.0.0.1';
-	    $port = '9905';
-	    $data = 'test';
-	    $timeout = 0.5; //second
-	    yield new Swoole\Client\TCP($ip, $port, $data, $timeout);
-	 }
-
-	  public function udpTest(){
-	    $ip = '127.0.0.1';
-	    $port = '9905';
-	    $data = 'test';
-	    $timeout = 0.5; //second
-	    yield new Swoole\Client\UDP($ip, $port, $data, $timeout);
-	  }
-
-	  public function httpTest(){
-	    $url='http://www.qq.com';
-	    $httpRequest= new Swoole\Client\HTTP($url);
-	    $data='testdata';
-	    $header = array(
-	      'Content-Length' => 12345,
-	    );
-	    yield $httpRequest->get($url); //yield $httpRequest->post($path, $data, $header);
-	  }
-	  
-	  
-	  public function muticallTest(){
-	    $ip = '127.0.0.1';
-	    $port = '9905';
-	    $data = 'test';
-	    $timeout = 0.5; //second
-	    $calls=new Swoole\Client\Multi();
-	    $firstReq=new Swoole\Client\TCP($ip, $port, $data, $timeout);
-	    $secondReq=new Swoole\Client\UDP($ip, $port, $data, $timeout);
-	    $calls ->request($firstReq,'first');             //first request
-	    $calls ->request($secondReq,'second');             //second request
-	    yield $calls;
-	  }
-
-
-
 	private function test(){
 
 		$test  = new TestModel();
-		$res = (yield $test ->testClient());
+		$res = (yield $test ->udpTest());
 		SysLog::info(__METHOD__ . " res == " .print_r($res, true), __CLASS__);
 		if ($res['r'] == 0) {
 
@@ -91,7 +36,7 @@ class TestController extends Controller{
 			yield array(
 				'r' => 1,
 				'error_msg' => 'yield failed',
-				);
+				 );
 		}
 	}
 }
--- examples/src/model/TestModel.php
+++ examples/src/model/TestModel.php
@@ -3,7 +3,7 @@
  * @Author: winterswang
  * @Date:   2015-07-03 18:10:05
  * @Last Modified by:   winterswang
- * @Last Modified time: 2015-07-03 19:57:47
+ * @Last Modified time: 2015-07-04 22:15:47
  */
 
 
@@ -14,7 +14,7 @@ class TestModel {
 		yield array('r' => 0, 'data' => 'yield test');
 	}
 
-	public function testClient(){
+	public function udpTest(){
 
 		// send data to back server 
 		$ip = '127.0.0.1';
@@ -23,4 +23,40 @@ class TestModel {
 		$timeout = 0.5; //second
 		yield new Swoole\Client\UDP($ip, $port, $data, $timeout);
 	}
+
+	public function httpTest(){
+
+	    $url='http://www.qq.com';
+	    $httpRequest= new Swoole\Client\HTTP($url);
+	    $data='testdata';
+	    $header = array(
+	      'Content-Length' => 12345,
+	    );
+	    yield $httpRequest->get($url); 
+	    //yield $httpRequest->post($path, $data, $header);
+	  }
+	  
+	public function muticallTest(){
+	    $ip = '127.0.0.1';
+	    $data = 'test';
+	    $timeout = 0.5; //second
+
+	    $calls=new Swoole\Client\Multi();
+
+	    $firstReq=new Swoole\Client\TCP($ip, '9905', $data, $timeout);
+	    $secondReq=new Swoole\Client\UDP($ip, '9904', $data, $timeout);
+
+	    $calls ->request($firstReq,'first');             //first request
+	    $calls ->request($secondReq,'second');             //second request
+
+	    yield $calls;
+	  }
+
+	public function tcpTest(){
+	    $ip = '127.0.0.1';
+	    $port = '9905';
+	    $data = 'test';
+	    $timeout = 0.5; //second
+	    yield new Swoole\Client\TCP($ip, $port, $data, $timeout);
+	 }
 }
