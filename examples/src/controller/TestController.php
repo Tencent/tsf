e2b8d5e0ab18439b16da83671a8b501fbe39a6f5
markyuan@tencent.com <syyuanyizhi@163.com>
2015-07-08 23:05:04 +0800

    Update TestController.php
    temp

--- examples/src/controller/TestController.php
+++ examples/src/controller/TestController.php
@@ -39,4 +39,35 @@ class TestController extends Controller{
 				 );
 		}
 	}
+	
+		public function actionMutical(){
+
+		$response = $this ->argv['response'];
+		$ip = '127.0.0.1';
+		$data = 'test';
+		$timeout = 0.5; //second
+		$calls=new Swoole\Client\Multi();
+		$firstReq=new Swoole\Client\TCP($ip, '9905', $data, $timeout);
+		$secondReq=new Swoole\Client\UDP($ip, '9904', $data, $timeout);
+		$calls ->request($firstReq,'first');             //first request
+		$calls ->request($secondReq,'second');             //second request
+		$ret=(yield $calls);
+		$response ->end(" test response ");
+
+	}
+
+
+	public function actionTest(){
+		$response = $this ->argv['response'];
+		$ip = '127.0.0.1';
+		$port = '9905';
+		$data = 'test';
+		$timeout = 0.5; //second
+		$ret=(yield new Swoole\Client\UDP($ip, $port, $data, $timeout));
+		$ret=(yield new Swoole\Client\TCP($ip, $port, $data, $timeout));
+		$url='http://www.qq.com';
+		$httpRequest= new Swoole\Client\HTTP($url);
+		$ret=(yield $httpRequest->get($url)); //yield $httpRequest->post($path, $data, $header);
+		$response ->end(" test response ");
+	}
 }
