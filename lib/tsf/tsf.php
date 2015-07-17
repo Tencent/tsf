84e4a834832e1fe98d6982dfd14d1757140f59ad
zangzhida <zang@zangzhidas-Air.lan>
2015-07-17 21:25:06 +0800

    modified old name in comments

--- lib/tsf/tsf.php
+++ lib/tsf/tsf.php
@@ -9,7 +9,7 @@
 //实现createwebapplication 加载tsf的代码 返回一个建好的类
 
 
-//todo    autoload psf 和 src里面的所有代码
+//todo    autoload tsf 和 src里面的所有代码
 define('TSFBASEPATH', dirname(dirname(__FILE__)));
 require_once 'AutoLoad.php';
 require_once TSFBASEPATH.'/Swoole/require.php';        //添加swoole的代码
